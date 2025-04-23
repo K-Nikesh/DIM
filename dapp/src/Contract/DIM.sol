03cdbdcb23a4252732f116011dcd345b7083e67e
K-Nikesh <kingofnikey2004@gmail.com>
2025-04-24 00:54:53 +0530

    new crt sol added

--- dapp/src/Contract/DIM.sol
+++ dapp/src/Contract/DIM.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.21;
 
 contract DecentralizedIM {
+    
     struct Credential {
         string credentialHash; // IPFS or off-chain hash
         address issuer;
@@ -26,24 +27,17 @@ contract DecentralizedIM {
     mapping(address => Identity) public identities;
     mapping(address => bool) public approvedIssuers;
     mapping(address => Credential[]) public credentials;
-    mapping(address => CredentialRequest[]) public credentialRequests; // issuer => list of requests
+    mapping(address => CredentialRequest[]) public credentialRequests; // issuer => requests
 
     address public admin;
 
     event IdentityRegistered(address user, string metadata);
-    event CredentialIssued(
-        address indexed user,
-        address issuer,
-        string credentialHash
-    );
+    event CredentialRequested(address indexed requester, address indexed issuer, string requestData);
+    event CredentialIssued(address indexed user, address issuer, string credentialHash);
     event CredentialRevoked(address indexed user, uint256 index);
     event IssuerApproved(address issuer);
     event IssuerRevoked(address issuer);
-    event CredentialRequested(
-        address indexed requester,
-        address indexed issuer,
-        string requestData
-    );
+    event CredentialRequestReviewed(address indexed issuer, uint256 index, bool approved);
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin");
@@ -86,88 +80,70 @@ contract DecentralizedIM {
         emit IssuerRevoked(issuer);
     }
 
-    
+    function requestCredential(address issuer, string memory requestData) external onlyRegistered {
+        require(approvedIssuers[issuer], "Issuer not approved");
+
+        credentialRequests[issuer].push(CredentialRequest({
+            requestData: requestData,
+            requester: msg.sender,
+            requestedAt: block.timestamp,
+            isApproved: false,
+            isReviewed: false
+        }));
+
+        emit CredentialRequested(msg.sender, issuer, requestData);
+    }
+
+    function approveRequest(uint256 index, string memory credentialHash) external onlyIssuer {
+        require(index < credentialRequests[msg.sender].length, "Invalid index");
+
+        CredentialRequest storage req = credentialRequests[msg.sender][index];
+        require(!req.isReviewed, "Already reviewed");
+
+        req.isReviewed = true;
+        req.isApproved = true;
+
+        credentials[req.requester].push(Credential({
+            credentialHash: credentialHash,
+            issuer: msg.sender,
+            issuedAt: block.timestamp,
+            isRevoked: false
+        }));
+
+        emit CredentialIssued(req.requester, msg.sender, credentialHash);
+        emit CredentialRequestReviewed(msg.sender, index, true);
+    }
+
+    function rejectRequest(uint256 index) external onlyIssuer {
+        require(index < credentialRequests[msg.sender].length, "Invalid index");
+
+        CredentialRequest storage req = credentialRequests[msg.sender][index];
+        require(!req.isReviewed, "Already reviewed");
+
+        req.isReviewed = true;
+        emit CredentialRequestReviewed(msg.sender, index, false);
+    }
 
     function revokeCredential(address user, uint256 index) external {
         require(index < credentials[user].length, "Invalid index");
-        require(
-            msg.sender == credentials[user][index].issuer ||
-                msg.sender == admin,
-            "Not authorized"
-        );
+        require(msg.sender == credentials[user][index].issuer || msg.sender == admin, "Not authorized");
 
         credentials[user][index].isRevoked = true;
 
         emit CredentialRevoked(user, index);
     }
 
-    function getCredentials(address user)
-        external
-        view
-        returns (Credential[] memory)
-    {
+    function getCredentials(address user) external view returns (Credential[] memory) {
         return credentials[user];
     }
 
-    function isCredentialValid(address user, uint256 index)
-        external
-        view
-        returns (bool)
-    {
+    function getCredentialRequests(address issuer) external view returns (CredentialRequest[] memory) {
+        return credentialRequests[issuer];
+    }
+
+    function isCredentialValid(address user, uint256 index) external view returns (bool) {
         if (index >= credentials[user].length) return false;
         Credential memory cred = credentials[user][index];
         return !cred.isRevoked;
-    }
-
-    function requestCredential(address issuer, string memory requestData)
-        external
-        onlyRegistered
-    {
-        require(approvedIssuers[issuer], "Issuer not approved");
-
-        credentialRequests[issuer].push(
-            CredentialRequest({
-                requestData: requestData,
-                requester: msg.sender,
-                requestedAt: block.timestamp,
-                isApproved: false,
-                isReviewed: false
-            })
-        );
-
-        emit CredentialRequested(msg.sender, issuer, requestData);
-    }
-
-    function approveRequest(uint256 index, string memory credentialHash)
-        external
-        onlyIssuer
-    {
-        require(index < credentialRequests[msg.sender].length, "Invalid index");
-        CredentialRequest storage req = credentialRequests[msg.sender][index];
-        require(!req.isReviewed, "Already reviewed");
-
-        req.isApproved = true;
-        req.isReviewed = true;
-
-        // Issue the credential
-        credentials[req.requester].push(
-            Credential({
-                credentialHash: credentialHash,
-                issuer: msg.sender,
-                issuedAt: block.timestamp,
-                isRevoked: false
-            })
-        );
-
-        emit CredentialIssued(req.requester, msg.sender, credentialHash);
-    }
-
-    function rejectRequest(uint256 index) external onlyIssuer {
-        require(index < credentialRequests[msg.sender].length, "Invalid index");
-        CredentialRequest storage req = credentialRequests[msg.sender][index];
-        require(!req.isReviewed, "Already reviewed");
-
-        req.isReviewed = true;
-        // Don't issue a credential
     }
 }
